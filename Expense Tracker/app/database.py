4c16dcca296991c59b26a5724c8b1e3c2f571983
Oak <kristapsimportant@gmail.com>
2025-10-10 01:55:53 -0700

    Update database.py
    Edited code, for more experimenting and learning purposes

--- Expense Tracker/app/database.py
+++ Expense Tracker/app/database.py
@@ -1,21 +1,21 @@
 from sqlalchemy import create_engine, text
 from sqlalchemy.dialects.oracle.dictionary import all_tables
+from sqlalchemy.engine import row
 
 engine = create_engine('sqlite:///:memory:', echo=True)
 
 with engine.connect() as conn:
-    conn.execute(text("CREATE TABLE table_example (x int, y int)"))
+    conn.execute(text("CREATE TABLE table_example (x, y)"))
 
-    conn.execute(
-        text("INSERT INTO table_example (x, y) VALUES (:x, :y)"),
-    [{"x": 1, "y": 1}, {"x": 3, "y": 4}],
-    )
-
+    conn.execute(text("INSERT INTO table_example (x,y) VALUES (:x,:y)"),
+                 [{"x": 4, "y": 5}, {"x": 6, "y": 7}],
+                 )
     conn.commit()
-All_TABLE = SELECT
-print(ALL_TABLE)
-if "x" in ALL_TABLE > 3:
-    print("X is more then 0")
+
+    result = conn.execute(text("SELECT x, y from table_example WHERE y = :y"), {"y":7})
+    for row in result:
+        print(f"x: {row.x} and y: {row.y}")
+
 
 
 
