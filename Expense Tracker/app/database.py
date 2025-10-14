10790d689b60ec1ee2b42bca84e14ee112b4a636
Oak <kristapsimportant@gmail.com>
2025-10-14 15:10:52 +0100

    Update database.py
    Tested more, leant more. How to filter, how to query, ect.

--- Expense Tracker/app/database.py
+++ Expense Tracker/app/database.py
@@ -28,7 +28,25 @@ class person(Base):
         return f"({self.ssn}), {self.firstname}, {self.lastname} ({self.gender}), {self.age})"
     
 
-engine = create_engine("sqlite:///:memory:", echo=True)
+class Thing(Base):
+    __tablename__ = "things"
+
+    tid = Column("tid", Integer, primary_key=True)
+    description = Column("Description", String)
+    owner = Column(Integer, ForeignKey("person.ssn"))
+
+    def __init__(self, tid, description, owner):
+        self.tid = tid
+        self.description = description
+        self.owner = owner
+
+    def __repr__(self):
+        return f"({self.tid}) {self.description} owned by {self.owner}"
+        
+
+
+
+engine = create_engine("sqlite:///database.db", echo=True)
 
 Base.metadata.create_all(bind=engine)
 
@@ -46,8 +64,25 @@ p2 = person(545677, "Alan", "Smiles", "M", 25)
 p3 = person(755434, "Ava", "Smith", "F", 23)
 p4 = person(65432, "Maria", "sim", "F", 43)
 
+
 session.add_all([p1,p2,p3,p4])
-session.commit
+session.commit()
 
 results = session.query(person).all()
 print(results)
+
+
+
+t1 = Thing(1, "Car", p0.ssn)
+t2 = Thing(2, "Gaming PC", p1.ssn)
+t3 = Thing(3, "20000 Student Debt", p2.ssn)
+t4 = Thing(4, "Book", p3.ssn)
+t5 = Thing(5, "A cat", p4.ssn)
+
+session.add_all([t1,t2,t3,t4,t5])
+session.commit()
+
+resultsthing = session.query(Thing, person).filter(Thing.owner == person.ssn).filter(person.firstname == "Ava").all()
+for r in resultsthing:
+    print(r)
+
