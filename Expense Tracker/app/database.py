59de6fa1a24b258115d105839012bf487dc634d1
Oak <kristapsimportant@gmail.com>
2025-10-14 14:50:49 +0100

    Update database.py
    just learning so far, classes right now

--- Expense Tracker/app/database.py
+++ Expense Tracker/app/database.py
@@ -1,21 +1,53 @@
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text, ForeignKey, Column, Integer, String, CHAR
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.dialects.oracle.dictionary import all_tables
 from sqlalchemy.engine import row
 
-engine = create_engine('sqlite:///:memory:', echo=True)
+Base = declarative_base()
 
-with engine.connect() as conn:
-    conn.execute(text("CREATE TABLE table_example (x, y)"))
+class person(Base):
+    __tablename__ = 'person'
 
-    conn.execute(text("INSERT INTO table_example (x,y) VALUES (:x,:y)"),
-                 [{"x": 4, "y": 5}, {"x": 6, "y": 7}],
-                 )
-    conn.commit()
+    ssn = Column('ssn', Integer, primary_key=True)
+    firstname = Column('firstname', String)
+    lastname = Column('lastname', String)
+    gender = Column('gender', CHAR)
+    age = Column('age', Integer)\
+    
 
-    result = conn.execute(text("SELECT x, y from table_example WHERE y = :y"), {"y":7})
-    for row in result:
-        print(f"x: {row.x} and y: {row.y}")
+    def __init__(self, ssn, firstname, lastname, gender, age):
+        self.ssn = ssn
+        self.firstname = firstname
+        self.lastname = lastname
+        self.gender = gender
+        self.age = age
 
+    
+    def __repr__(self):
+        return f"({self.ssn}), {self.firstname}, {self.lastname} ({self.gender}), {self.age})"
+    
 
+engine = create_engine("sqlite:///:memory:", echo=True)
 
+Base.metadata.create_all(bind=engine)
 
+Session = sessionmaker(bind=engine)
+session = Session()
+
+p0 = person(3263345, "Bob", "Dillian", "M", 35)
+
+session.add(p0)
+
+session.commit()
+
+p1 = person(4363, "Hillian", "Dillian", "M", 35)
+p2 = person(545677, "Alan", "Smiles", "M", 25)
+p3 = person(755434, "Ava", "Smith", "F", 23)
+p4 = person(65432, "Maria", "sim", "F", 43)
+
+session.add_all([p1,p2,p3,p4])
+session.commit
+
+results = session.query(person).all()
+print(results)
