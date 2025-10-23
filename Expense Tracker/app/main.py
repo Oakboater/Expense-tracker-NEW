79b4c4d3208278a8bd1958d063c6fc75c7852257
Oak <kristapsimportant@gmail.com>
2025-10-23 06:42:08 -0700

    Add endpoints to retrieve people and totals

--- Expense Tracker/app/main.py
+++ Expense Tracker/app/main.py
@@ -1,8 +1,35 @@
 from fastapi import FastAPI
-
+from database import session, Person, Expense, Category, Thing
+from sqlalchemy import func
 app = FastAPI()
 
 @app.get("/")
 
 def read_roots():
     return {"boot up complete": "Tracker API is running!"}
+
+
+@app.get("/people")
+def get_people():
+    people = session.query(Person).all()
+    return [
+        {
+            "ssn": p.ssn,
+            "firstname": p.firstname,
+            "lastname": p.lastname,
+            "gender": p.gender,
+            "age": p.age
+        }
+        for p in people
+    ]
+
+@app.get("totals")
+def get_totals():
+    totals = (
+        session.query(Person.firstname, func.sum(Expense.amount))
+        .join(Expense, Expense.owner == Person.ssn)
+        .group_by(Person.firstname)
+        .all()
+    )
+    return [{"person": name, "total_spent": total} for name, total in totals]
+
