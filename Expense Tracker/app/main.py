20c7568d0b0490ed2fcd23045e1e75beeb1d28f0
Oak <kristapsimportant@gmail.com>
2025-11-03 01:54:44 -0800

    Add endpoints for creating person and expense

--- Expense Tracker/app/main.py
+++ Expense Tracker/app/main.py
@@ -1,7 +1,54 @@
 from fastapi import FastAPI
 from database import session, Person, Expense, Category, Thing
 from sqlalchemy import func
+from schemas import PersonCreate, ExpenseCreate
+from datetime import datetime
+
 app = FastAPI()
+
+
+
+# database
+def get_db():
+    db = session()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/")
+def read_root():
+    return {"WELCOME"}
+
+
+
+@app.post("/people")
+def create_person(person: PersonCreate):
+    new_person = Person(
+        ssn=person.ssn,
+        firstname=person.firstname,
+        lastname=person.lastname,
+        gender=person.gender,
+        age=person.age
+    )
+    session.add(new_person)
+    session.commit()
+    return {"Message": f"Person {person.firstname} added successfully"}
+
+@app.post("/expenses")
+def create_expense(expense: ExpenseCreate):
+    new_expense = Expense(
+        tid=expense.tid,
+        amount=expense.amount,
+        item=expense.item,
+        category=expense.category,
+        owner=expense.owner,
+        category_id=expense.category_id,
+        date=expense.datetime.now()
+    )
+    session.add(new_expense)
+    session.commit()
+    return {"Message": f"Expense {expense.item} added successfully"}
 
 @app.get("/")
 
@@ -23,7 +70,7 @@ def get_people():
         for p in people
     ]
 
-@app.get("totals")
+@app.get("/totals")
 def get_totals():
     totals = (
         session.query(Person.firstname, func.sum(Expense.amount))
@@ -32,4 +79,5 @@ def get_totals():
         .all()
     )
     return [{"person": name, "total_spent": total} for name, total in totals]
+
 
