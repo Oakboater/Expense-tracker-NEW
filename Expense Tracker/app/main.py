257893ef7f88abcb95b93ba6e5973f5a4e6ff88b
Oak <kristapsimportant@gmail.com>
2025-11-06 06:14:31 -0800

    Refactor expense retrieval and creation endpoints

--- Expense Tracker/app/main.py
+++ Expense Tracker/app/main.py
@@ -1,6 +1,7 @@
+from typing import List
 from fastapi import FastAPI, Depends
 from .database import session, Person, Expense
-from .schemas import PersonCreate, ExpenseCreate
+from .schemas import PersonCreate, ExpenseCreate, ExpenseOut
 from sqlalchemy import func
 from datetime import datetime
 
@@ -39,17 +40,15 @@ def get_people(db: session = Depends(get_db)):
     ]
 
 
-@app.get("/expenses/{user_id}")
+@app.get("/expenses/{user_id}", response_model=List[ExpenseOut])
 def get_expense(user_id: int, db: session = Depends(get_db)):
     expense = db.query(Expense).filter(Expense.owner == user_id).all()
-
     return [
         {
         "Item": e.item,
         "Cost": e.cost,
         "date": e.date,
-        "category": e.category_rel.name if e.category_rel else None,
-        "category_id": e.category_id
+        "category":e.category,
         }
         for e in expense
     ]
@@ -82,8 +81,8 @@ def create_expense(expense: ExpenseCreate, db: session = Depends(get_db)):
         cost=expense.cost,
         item=expense.item,
         owner=expense.owner,
-        category_id=expense.category_id,
-        date=datetime.now() or expense.date
+        category=expense.category,
+        date=expense.date or datetime.now()
     )
     db.add(new_expense)
     db.commit()
